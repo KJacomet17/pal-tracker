cec802011d8d10b137f2cc6ce2b455f75b8296ac
Ubuntu <ubuntu@ip-172-31-85-219.ec2.internal>
2020-12-17 18:36:51 +0000

    Day4 Lab1: Spring JDBC Tempate

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,14 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRespository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
